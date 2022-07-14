26e9f05fa203ff9e20ee1a6e8ea2414e5da86d38
krollkim <kimkroll2000@gmail.com>
2022-07-14 20:29:15 +0300

    updating nav-bar animation

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const hamburger = document.querySelector(".header .nav-bar .nav-list .hamburger");
 const mobile_menu = document.querySelector(".header .nav-bar .nav-list ul");
-const header = document.querySelector(".header .container");
+const header = document.querySelector(".header.container ");
+
 
 hamburger.addEventListener("click", () =>{
     hamburger.classList.toggle('active');
@@ -14,5 +15,5 @@ document.addEventListener('scroll', () =>{
     }else{
       header.style.backgroundColor = 'transparent';
     }
-       
+      console.log(header) 
 })
