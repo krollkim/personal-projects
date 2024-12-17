97ba8ab3e85ddab15b7a7c297037ba8145f69ee6
krollkim <kimkroll2000@gmail.com>
2024-12-18 03:27:02 +0700

    responsivness, design, info and general fixes

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const hamburger = document.querySelector(".header .nav-bar .nav-list .hamburger");
 const mobile_menu = document.querySelector(".header .nav-bar .nav-list ul");
 const header = document.querySelector(".header.container ");
+const menu_links = document.querySelectorAll(".header .nav-bar .nav-list ul li a");
 
 
 hamburger.addEventListener("click", () =>{
@@ -17,3 +18,10 @@ document.addEventListener('scroll', () =>{
     }
       console.log(header) 
 })
+
+menu_links.forEach(link => {
+  link.addEventListener("click", () => {
+    hamburger.classList.remove('active');
+    mobile_menu.classList.remove('active');
+  });
+});
